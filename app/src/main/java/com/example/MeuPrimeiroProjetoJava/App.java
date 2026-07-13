4c33c9d0e1ab67601352fb40d7593dce1d923149
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-13 19:53:49 -0300

    adiciona estudos de estruturas condicionais

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -3,6 +3,23 @@ package com.example.MeuPrimeiroProjetoJava;
 public class App {
 
     public static void main(String[] args) {
- 
+        
+        /*
+         e => &&
+         ou => ||
+         negação => !
+         */
+
+        int media = 7;
+
+        if (media >= 7) {
+            if (media == 10) {
+                // aprovado com nota máxima
+            } else {
+                // aprovado
+            }
+        } else {
+            // reprovado
+        }
     }
 }
