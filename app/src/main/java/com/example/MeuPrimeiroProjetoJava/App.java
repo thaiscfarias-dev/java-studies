27552d6cab9f588d1e21e5189886f55da1c811d5
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-13 20:56:37 -0300

    exercicio simples de if e else

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,21 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+        int numero;
+
+        Scanner leitorScanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        numero = leitorScanner.nextInt();
+
+        if (numero >= 100 && numero <= 200) {
+            System.out.println("O número está no intervalo");
+        } else {
+            System.out.println("O número não está no intervalo");
+        }
     }
 }
