f5103b088e076ee598fde9e236a71d204c06281e
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-13 20:00:33 -0300

    adiciona estudo de switch case

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -3,6 +3,16 @@ package com.example.MeuPrimeiroProjetoJava;
 public class App {
 
     public static void main(String[] args) {
- 
+        int codigoProduto = 2;
+
+        switch (codigoProduto) {
+            case 1:
+                // todo case tem que ter um break
+                break;
+            case 2:
+                break;
+            // caso nenhum dos cases seja valido
+            default:
+        }
     }
 }
