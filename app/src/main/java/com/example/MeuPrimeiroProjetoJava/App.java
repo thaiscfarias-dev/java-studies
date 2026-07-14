028538ebc6fe1eb8525c0be72f7a6d099ac91ffc
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-13 21:08:42 -0300

    adiciona estudo de while e for

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -3,6 +3,16 @@ package com.example.MeuPrimeiroProjetoJava;
 public class App {
 
     public static void main(String[] args) {
- 
+
+        int totalAlunos = 0;
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("For: " + i);
+        }
+
+        while (totalAlunos < 10) {
+            totalAlunos++;
+            System.out.println("While: " + totalAlunos);
+        }
     }
 }
