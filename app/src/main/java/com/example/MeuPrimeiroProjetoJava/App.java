f571cc24e2875b55c7efbb8cf900e6398b295918
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-13 21:23:09 -0300

    adiciona exercicio estrutura repeticao

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,32 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+        Scanner leitorScanner = new Scanner(System.in);
+
+        char desejaContinuar = 'S';
+
+        while (desejaContinuar == 'S' || desejaContinuar == 's') {
+            System.out.println("Digite um numero");
+            int numero = leitorScanner.nextInt();
+
+            if (numero == 0) {
+                System.out.println("O numero é zero");
+            } else {
+                if (numero > 0) {
+                    System.out.println("O numero é maior que zero");
+                } else {
+                    System.out.println("O numero é menor que zero");
+                }
+            }
+
+            System.out.println("Deseja continuar? S- sim / N - não");
+            desejaContinuar = leitorScanner.next().charAt(0);
+
+        }
+
     }
 }
