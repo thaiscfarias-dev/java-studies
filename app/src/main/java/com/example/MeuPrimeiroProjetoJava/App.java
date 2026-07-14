939381b83e97fc3b0af1e5d29ef56d85076073aa
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-14 14:30:27 -0300

    adiciona o codigo para calcular o imc

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,39 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        /* colocar o Scanner dentro do try garante que o Scanner será encerrado
+            assim que o bloco try fechar, consumindo menos memoria. Não precisa
+            lembrar de digitar leitor.close();
+         */
+        try (Scanner leitor = new Scanner(System.in)) {
+
+            IMC imc = new IMC();
+            boolean continuarTentando = true;
+
+            while (continuarTentando) {
+
+                try {
+
+                    System.out.println("digite o peso: ");
+                    imc.peso = leitor.nextFloat();
+                    System.out.println("digite a altura: ");
+                    imc.altura = leitor.nextFloat();
+                    System.out.println("o IMC e: " + imc.calcularIMC());
+
+                    continuarTentando = false;
+
+                } catch (InputMismatchException e) {
+                    System.out.println("entrada invalida. Tente novamente");
+                    leitor.next();
+                }
+            }
+        }
+
     }
 }
