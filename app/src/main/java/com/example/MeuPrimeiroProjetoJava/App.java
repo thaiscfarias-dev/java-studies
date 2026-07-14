49d2aa4b0d6863437d0c4394807ac73af5e2d395
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-13 21:51:08 -0300

    adiciona exercicio de estrutura de repeticao for

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,52 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        String nomeProduto;
+        float precoCusto;
+        float precoVenda;
+        float totalCusto = 0.0f;
+        float totalVenda = 0.0f;
+
+        Scanner leitor = new Scanner(System.in);
+        
+        int i = 0;
+        for (; i < 2; i++) {
+            System.out.println("Digite o nome do produto: ");
+            nomeProduto = leitor.next();
+
+            System.out.println("Digite o preço de custo do produto: ");
+            precoCusto = leitor.nextFloat();
+
+            System.out.println("Digite o preco de venda do produto: ");
+            precoVenda = leitor.nextFloat();
+            
+            totalCusto = totalCusto + precoCusto;
+            totalVenda = totalVenda + precoVenda;
+
+            if (precoCusto == precoVenda) {
+                System.out.println("Empate");
+            } else {
+                if (precoCusto > precoVenda) {
+                    System.out.println("Prejuizo");
+
+                } else {
+                    System.out.println("Lucro");
+
+                }
+            }
+           
+            System.out.println(nomeProduto + ", preco de custo = " +
+                    precoCusto + ", preco de venda = " + precoVenda);
+            
+        }
+        
+        System.out.println("A media do preco de custo e de: " + (totalCusto)/i + 
+                ", e do preco de venda e de: " + (totalVenda)/i);
+
     }
 }
