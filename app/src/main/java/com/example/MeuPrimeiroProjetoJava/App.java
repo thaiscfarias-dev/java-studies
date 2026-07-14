43cbabc325e3f27e39b2110bf0ac4a5551011ee3
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-14 11:38:43 -0300

    remove comentários

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -6,44 +6,44 @@ public class App {
 
     public static void main(String[] args) {
 
-        /* exibir o nome do usuário */
-        Nome nome = new Nome();
+        try (Scanner leitor = new Scanner(System.in)) {
+            /* exibir o nome do usuário */
+            Nome nome = new Nome();
+            
+            System.out.println("Digite seu nome: ");
+            nome.nome = leitor.nextLine();
 
-        Scanner leitor = new Scanner(System.in);
+            System.out.println(nome.cumprimentarPessoa());
 
-//        System.out.println("Digite seu nome: ");
-//        nome.nome = leitor.next();
-//        
-//        System.out.println(nome.cumprimentarPessoa());
+            /* soma de dois numeros */
+            Soma soma = new Soma();
+            System.out.println("Vamos somar dois numeros!");
+            System.out.println("Digite o primeiro numero: ");
+            soma.primeroNumero = leitor.nextFloat();
 
-        /* soma de dois numeros */
-//        Soma soma = new Soma();
-//        System.out.println("Vamos somar dois numeros!");
-//        System.out.println("Digite o primeiro numero: ");
-//        soma.primeroNumero = leitor.nextFloat();
-//        
-//        System.out.println("Digite o segundo numero: ");
-//        soma.segundoNumero = leitor.nextFloat();
-//        
-//        System.out.println("A soma dos dois numeros e: " + soma.somarNumeros());
+            System.out.println("Digite o segundo numero: ");
+            soma.segundoNumero = leitor.nextFloat();
 
-        /* média de dois numeros */
-//        Media media = new Media();
-//        System.out.println("Vamos calcular a media de dois numeros!");
-//        System.out.println("Digite o primeiro numero: ");
-//        media.primeroNumero = leitor.nextFloat();
-//
-//        System.out.println("Digite o segundo numero: ");
-//        media.segundoNumero = leitor.nextFloat();
-//
-//        System.out.println("A media dos dois numeros e: " + media.calcularMedia());
+            System.out.println("A soma dos dois numeros e: " + soma.somarNumeros());
 
-        /* conversão de Celsius para Fahrenheit */
-        Temperatura temperatura = new Temperatura();
-        System.out.println("Qual temperatura em Celsius voce deseja converter? ");
-        
-        temperatura.celsius = leitor.nextFloat();
-        System.out.println("A temperatura em Fahrenheit e: " 
-                + temperatura.converterTemperatura());
+            /* média de dois numeros */
+            Media media = new Media();
+            System.out.println("Vamos calcular a media de dois numeros!");
+            System.out.println("Digite o primeiro numero: ");
+            media.primeroNumero = leitor.nextFloat();
+
+            System.out.println("Digite o segundo numero: ");
+            media.segundoNumero = leitor.nextFloat();
+
+            System.out.println("A media dos dois numeros e: " + media.calcularMedia());
+
+            /* conversão de Celsius para Fahrenheit */
+            Temperatura temperatura = new Temperatura();
+            System.out.println("Qual temperatura em Celsius voce deseja converter? ");
+
+            temperatura.celsius = leitor.nextFloat();
+            System.out.println("A temperatura em Fahrenheit e: "
+                    + temperatura.converterTemperatura());
+        }
     }
 }
