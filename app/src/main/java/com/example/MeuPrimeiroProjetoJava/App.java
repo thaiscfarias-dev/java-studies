4f528f754c193cb6ed271bb6b7c0ed376b2bb338
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-14 11:07:27 -0300

    adiciona classe nome

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,18 @@
 package com.example.MeuPrimeiroProjetoJava;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+        
+        Nome nome = new Nome();
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite seu nome: ");
+        nome.nome = leitor.next();
+        
+        System.out.println(nome.cumprimentarPessoa());
  
     }
 }
---
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/Nome.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.MeuPrimeiroProjetoJava;
+
+/**
+ *
+ * @author DELL
+ */
+public class Nome {
+    
+    String nome;
+    
+    public String cumprimentarPessoa(){
+        String cumprimento = "Hello, " + nome + " seja bem-vinda ao mundo Java";
+        return cumprimento;
+    }
+    
+}
