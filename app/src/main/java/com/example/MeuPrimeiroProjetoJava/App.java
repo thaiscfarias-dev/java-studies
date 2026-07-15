582d4b35245aa9a7bb863b09647328ed6e74f664
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-14 22:44:38 -0300

    adiciona resolucao do exercicio para descobrir se um numero é primo ou nao

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,50 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        int numero;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("digite um numero para saber se eh primo: ");
+            numero = scanner.nextInt();
+
+            if (ehPrimo(numero)) {
+                System.out.println("eh primo!");
+            } else {
+                System.out.println("nao eh primo!");
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("entrada invalida");
+        }
+
+    }
+
+    public static boolean ehPrimo(int numero) {
+
+        if (numero <= 1) {
+            return false;
+        }
+
+        if (numero == 2) {
+            return false;
+        }
+
+        if (numero % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i <= Math.sqrt(numero); i += 2) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
