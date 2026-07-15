863f16fa21a214ad96b19a37439b746d55e6693a
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-14 22:10:38 -0300

    adiciona a logica for para calcular a tabuada

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,27 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        int numero;
+        int resultado;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("digite um numero para a tabuada: ");
+            numero = scanner.nextInt();
+
+            for (int i = 0; i <= 10; i++) {
+                resultado = numero * i;
+                System.out.println(numero + " x " + i + " = " + resultado);
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("entrada invalida");
+        }
     }
 }
