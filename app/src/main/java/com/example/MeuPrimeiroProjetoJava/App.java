3eddc96a4a0178d7b387599f67fc001952dd384f
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-14 22:19:07 -0300

    adiciona soma de numeros de 1 a 100

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,17 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.stream.IntStream;
+
 public class App {
 
     public static void main(String[] args) {
- 
+        int soma;
+//        for (int i = 0; i <= 100; i++) {
+//            soma = soma + i;
+//        }
+
+        soma = IntStream.rangeClosed(1, 100).sum();
+        System.out.println("Soma de todos os numeros de 1 a 100: " + soma);
+
     }
 }
