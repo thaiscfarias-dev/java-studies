da08835e26d5f0638233244f2aaf38122634825c
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-16 15:36:24 -0300

    adiciona array e for para descobrir o maior, menor, media e soma de 5 numeros

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,48 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < numeros.length; i++) {
+                System.out.println("número: ");
+                numeros[i] = scanner.nextInt();
+            }
+
+            int maior = numeros[0];
+            int menor = numeros[0];
+            int soma = 0;
+            float media = 0;
+
+            for (int i = 1; i < numeros.length; i++) {
+
+                if (numeros[i] > maior) {
+                    maior = numeros[i];
+                }
+
+                if (numeros[i] < menor) {
+                    menor = numeros[i];
+                }
+            }
+
+            System.out.println("O maior número é: " + maior);
+            System.out.println("O menor número é: " + menor);
+
+            for (int i = 0; i < numeros.length; i++) {
+                soma = soma + numeros[i];
+            }
+            System.out.println("A soma é: " + soma);
+
+            media = (float) soma / numeros.length;
+            System.out.println("A média é: " + media);
+
+        } catch (InputMismatchException e) {
+        }
     }
 }
