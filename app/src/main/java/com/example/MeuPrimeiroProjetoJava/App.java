d9280f7f717d0f3858f916a6bd455059c66b6921
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-15 22:24:10 -0300

    adiciona método que soma dois numeros

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,31 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        float numero1;
+        float numero2;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("primeiro numero: ");
+            numero1 = scanner.nextFloat();
+
+            System.out.println("segundo numero: ");
+            numero2 = scanner.nextFloat();
+
+            System.out.println("a soma eh: " + somar(numero1, numero2));
+
+        } catch (InputMismatchException e) {
+            System.out.println("entrada invalida!");
+        }
+
+    }
+
+    private static float somar(float num1, float num2) {
+        return num1 + num2;
     }
 }
