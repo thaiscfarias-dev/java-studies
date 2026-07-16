3c975365891afadda44962f711ff0b9c9b5ceb35
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-16 17:20:48 -0300

    adiciona array e for para procurar numero

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,48 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        boolean encontrado = false;
+        int[] numeros = new int[5];
+        int numero;
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.printf("Digite o %dº número: ", i + 1);
+            numeros[i] = lerNumero(scanner);
+        }
+
+        System.out.println("Qual número deseja procurar? ");
+        numero = lerNumero(scanner);
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numero == numeros[i]) {
+                encontrado = true;
+                System.out.println("Número encontrado. Posição: " + (i + 1));
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("Número não encontrado.");
+        }
+        scanner.close();
+    }
+
+    public static int lerNumero(Scanner scanner) {
+        while (true) {
+            try {
+                return scanner.nextInt();
+
+            } catch (InputMismatchException e) {
+                System.out.println("Digite apenas números");
+                scanner.next();
+            }
+        }
     }
 }
