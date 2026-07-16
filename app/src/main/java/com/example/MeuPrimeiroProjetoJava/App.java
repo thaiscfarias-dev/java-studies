22656ac258eba13603129d4a62f4dcf901c9034d
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-16 16:45:44 -0300

    adiciona array para ordenar numeros

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,35 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < numeros.length; i++) {
+
+                try {
+                    System.out.printf("Digite o %dº número: ", i + 1);
+                    numeros[i] = scanner.nextInt();
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Digite apenas números!");
+                    scanner.next();
+                    i--;
+                }
+            }
+
+            Arrays.sort(numeros);
+            System.out.println("A ordem dos números é: "
+                    + Arrays.toString(numeros));
+
+        }
+
     }
 }
