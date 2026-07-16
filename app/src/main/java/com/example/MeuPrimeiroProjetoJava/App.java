b8c0858f5707a9e73fc392ca54250e3c4cc8807d
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-15 22:46:20 -0300

    adiciona metodo para verificar se um nummero é par ou ímpar

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,31 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("digite um numero: ");
+            int numero = scanner.nextInt();
+
+            if (isPar(numero)) {
+                System.out.println("par");
+            } else {
+                System.out.println("ímpar");
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("entrada invalida");
+        }
+
+    }
+
+    private static boolean isPar(int numero) {
+        return numero % 2 == 0;
     }
 }
