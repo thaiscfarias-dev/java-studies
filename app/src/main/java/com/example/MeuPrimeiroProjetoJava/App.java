8ed71327cf2ed35e2419c352aa555c041b418265
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-17 21:46:35 -0300

    adiciona array original e array invertido

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,43 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+        
+        try(Scanner scanner = new Scanner(System.in)){
+        
+            /* é uma convenção em java utilizar constantes em letras maiúsculas */
+            final int QUANTIDADE_NUMEROS = 5;
+            int[] numeros = new int[QUANTIDADE_NUMEROS];
+            int[] invertido = new int[numeros.length];
+            
+            for(int i = 0; i < numeros.length; i++){
+                System.out.printf("Digite o %dº número: ", (i + 1));
+                numeros[i] = lerNumero(scanner);
+            }
+            
+            for(int i = 0; i < numeros.length; i++){
+                invertido[i] = numeros[numeros.length - 1 - i];
+            }
+            
+            System.out.println("Original: " + Arrays.toString(numeros));
+            System.out.println("Invertido: " + Arrays.toString(invertido));
+        }
+
+    }
+
+    public static int lerNumero(Scanner scanner) {
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Digite apenas números");
+                scanner.next();
+            }
+        }
     }
 }
