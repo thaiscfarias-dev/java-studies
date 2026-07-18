a56d96814f6592d1d17d72cc9193eb1311a9d284
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-17 22:47:56 -0300

    adiciona classe menu e metodo exibir menu

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,44 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        Menu menu = new Menu();
+
+        try(Scanner scanner = new Scanner(System.in)){
+        
+            menu.exibir();
+            int opcao = lerNumero(scanner);
+
+            do {
+                switch (opcao) {
+                    case 1 ->
+                        menu.inserirNumeros();
+                    case 2 ->
+                        menu.procurarNumero();
+                    case 3 ->
+                        menu.mostrarTodos();
+                    default ->
+                        System.out.println("Até logo!");
+                }
+            } while (opcao != 4);
+        }
+
     }
+
+    public static int lerNumero(Scanner scanner) {
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Digite apenas números");
+                scanner.next();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/Menu.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.MeuPrimeiroProjetoJava;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class Menu {
+
+    public void exibir() {
+        System.out.println("""
+               Escolha uma opção:
+               1 - Inserir números
+               2 - Procurar número
+               3 - Mostrar todos
+               4 - Sair
+               """);
+    }
+
+    public int inserirNumeros() {
+        return 0;
+    }
+
+    public int procurarNumero() {
+        return 0;
+    }
+
+    public int mostrarTodos() {
+        return 0;
+    }
+
+}
