d59726fc32ba76cea88c7f84ce991db47e3504f5
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-18 23:30:07 -0300

    adiciona HashMap para contar letras em uma frase

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,32 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Scanner;
+import java.util.HashMap;
+
 public class App {
 
     public static void main(String[] args) {
- 
+        try (Scanner scanner = new Scanner(System.in)) {
+            String frase;
+            /* Um HashMap guarda informações em pares de chave e valor. */
+            HashMap<Character, Integer> contador = new HashMap<>();
+
+            System.out.println("Digite uma frase: ");
+            frase = scanner.nextLine();
+            frase = frase.toLowerCase().replace(" ", "");
+
+            for (int i = 0; i < frase.length(); i++) {
+
+                char letra = frase.charAt(i);
+
+                if (contador.containsKey(letra)) {
+                    contador.put(letra, contador.get(letra) + 1);
+                } else {
+                    contador.put(letra, 1);
+                }
+            }
+            System.out.println(contador);
+
+        }
     }
 }
