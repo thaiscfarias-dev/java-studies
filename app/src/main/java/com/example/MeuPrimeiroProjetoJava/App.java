29b7c873785e180c293d22e843f3599a5d485335
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-18 22:36:39 -0300

    adiciona chartAt e indexOf para descobrir quantas vogais tem em uma frase

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,34 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite uma frase: ");
+            String frase = scanner.nextLine();
+
+            int quantidadeVogais = 0;
+            String vogais = "aeiouáàéèíìóòúùäëïöüãõâêîôû";
+
+            frase = frase.toLowerCase();
+
+            for (int i = 0; i < frase.length(); i++) {
+                char letra = frase.charAt(i);
+
+              /*
+                 0 ou maior → encontrou o caractere.
+                -1 → não encontrou.
+              */
+                if (vogais.indexOf(letra) != -1) {
+                    quantidadeVogais++;
+                }
+            }
+
+            System.out.println("Quantidade de vogais: " + quantidadeVogais);
+        }
+
     }
 }
