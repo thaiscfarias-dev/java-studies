33c713d04bac429302ed34bd56126e4f1e5624b5
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-18 23:03:03 -0300

    adiciona contagem de palavras com loop for e com trim e split

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,39 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            String texto;
+            int quantidade = 1;
+
+            System.out.println("Digite um texto/frase: ");
+            texto = scanner.nextLine();
+
+            for (int i = 0; i < texto.length(); i++) {
+                if (texto.charAt(i) == ' ') {
+                    quantidade++;
+                }
+            }
+
+            System.out.println("Quantidade de palavras: " + quantidade);
+
+            /*
+            
+            split(" ") quebra a frase sempre que encontra um espaço
+            
+            O que significa \\s+?
+            \\s → qualquer espaço em branco (espaço, tabulação, quebra de linha).
+            + → um ou mais.
+
+            Então, não importa se houver 1, 5 ou 20 espaços entre as palavras.
+             */
+            String[] palavras = texto.trim().split("\\s+");
+            System.out.println("Quantidade de palavras método split: " + palavras.length);
+        }
+
     }
 }
