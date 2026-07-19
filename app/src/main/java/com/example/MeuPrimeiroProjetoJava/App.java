de28d9215685c51a9b817c923331b1c87d52e9da
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-18 22:50:30 -0300

    adiciona for para verificar se um texto ou palavra é um palíndromo

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,8 +1,32 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
- 
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            boolean palindromo = true;
+            String texto;
+
+            System.out.println("Digite um texto/palavra: ");
+            texto = scanner.nextLine();
+            texto = texto.toLowerCase().replace(" ", "");
+
+            for (int i = 0; i < texto.length() / 2; i++) {
+
+                if (texto.charAt(i) != texto.charAt(texto.length() - 1 - i)) {
+                    palindromo = false;
+                    break;
+                }
+            }
+
+            if (palindromo) {
+                System.out.println("É um palíndromo");
+            } else {
+                System.out.println("Não é um palíndromo");
+            }
+        }
     }
 }
