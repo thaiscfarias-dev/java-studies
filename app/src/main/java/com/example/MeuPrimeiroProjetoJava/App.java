b6fa55448ee584800d316dd9ef0654a78027c950
THAIS FARIAS <farias.thais@uol.com.br>
2026-07-19 22:50:19 -0300

    adiciona tratamento para os erros

--- app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
+++ app/src/main/java/com/example/MeuPrimeiroProjetoJava/App.java
@@ -1,16 +1,32 @@
 package com.example.MeuPrimeiroProjetoJava;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class App {
 
     public static void main(String[] args) {
-        
+
         Pessoa pessoa = new Pessoa();
-        
-        pessoa.nome = "Fulana";
-        pessoa.idade = 60;
-        pessoa.altura = 1.70;
-        
-        pessoa.apresentar();
- 
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            
+            try{
+                System.out.println("Digite um nome: ");
+                pessoa.nome = scanner.nextLine();
+
+                System.out.println("Digite uma idade: ");
+                pessoa.idade = scanner.nextInt();
+
+                System.out.println("Digite uma altura: ");
+                pessoa.altura = scanner.nextDouble();
+
+                pessoa.apresentar();
+            
+            }catch(InputMismatchException e){
+                System.out.println("Entrada inválida");
+                scanner.next();
+            }
+        }
     }
 }
